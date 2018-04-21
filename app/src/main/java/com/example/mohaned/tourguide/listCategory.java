b2b09f808e6956437a3f95a433fa86636db98d2f
Mohaned-Adel <mohaned.adel1996@gmail.com>
2018-04-22 00:24:37 +0200

    connect home with second page

---
+++ app/src/main/java/com/example/mohaned/tourguide/listCategory.java
@@ -0,0 +1,17 @@
+package com.example.mohaned.tourguide;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mohaned on 4/21/2018.
+ */
+
+public class listCategory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_categories);
+    }
+}
